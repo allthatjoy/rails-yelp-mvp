9bd0baa0d16e01096ccb55602469bd5c0e1ba429
Joy Seng <allthatjoy@gmail.com>
2018-08-08 21:34:05 +1000

    reviews form works

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,14 +4,31 @@ class ReviewsController < ApplicationController
     #send the restaurant object
     @restaurant = Restaurant.find(params[:restaurant_id])
   end
-  def create
-    #i will need a restaurant object to set the id
-    #you'll get it from the param (the id of the restauant)
+  # def create
+  #   #i will need a restaurant object to set the id
+  #   #you'll get it from the param (the id of the restauant)
 
+  #   @review = Review.new(review_param)
+  #   @review.restaurant = Restaurant.find(params[:restaurant_id])
+  #   @review.save
+  #   redirect_to restaurant_path(params[:restaurant_id])
+  # end
+    def create
+    #1 - Let's populate a new Review object witht the input from the user
     @review = Review.new(review_param)
-    @review.restaurant = Restaurant.find(params[:restaurant_id])
-    @review.save
+    #2 - Let's get the Restaurant this review is being created for
+    #i will need a restaurant object to set the id of the Review Object
+    #you'll get it from the param (the id of the restauant)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+
+    #3 - let's now set the @review.restaurant_id to the @restaurant.id
+    @review.restaurant = @restaurant
+    #let's save!
+    if @review.save
     redirect_to restaurant_path(params[:restaurant_id])
+    else
+      render :new
+    end
   end
 
     private
